ae33ae1bf1246b3d6f39604879dfffe88484b66b
DaEun Kim <rlaekdms901@naver.com>
2025-07-26 16:14:10 +0900

    Revert "feat: Login 화면 UI 1차 구현"

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,7 +4,6 @@ import Home from '../pages/Home';
 import HomePage from '../pages/HomePage';
 import RequestListPage from '../pages/request-list/RequestListPage';
 import ChooseCategoryPage from '../pages/request-write/ChooseCategoryPage';
-import Login from '../pages/Login';
 
 export default function Router() {
   return (
@@ -12,7 +11,6 @@ export default function Router() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/request/category" element={<ChooseCategoryPage />} />
         <Route path="/request-list" element={<RequestListPage />}>
