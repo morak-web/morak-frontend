68b66f8fdde6a1515400b9d0bfb455f8dbeb849e
daeun <rlaekdms901@naver.com>
2025-08-12 01:02:43 +0900

    feat: 의뢰서 직접 작성 페이지 UI 수정

--- src/pages/request-write/RequestWritePage.jsx
+++ src/pages/request-write/RequestWritePage.jsx
@@ -1,7 +1,24 @@
 import MainLayout from '../../components/layout/MainLayout';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import DatePicker from 'react-datepicker';
 
 export default function RequestWritePage() {
+  const [projectTitle, setProjectTitle] = useState('');
+  const [money, setMoney] = useState('');
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [detail, setDetail] = useState('');
+  const [warning, setWarning] = useState(false);
+
+  useEffect(() => {
+    if (!projectTitle || !money || !startDate || !endDate || !detail) {
+      setWarning(true);
+    } else {
+      setWarning(false);
+    }
+  }, [projectTitle, money, startDate, endDate, detail]);
+
   return (
     <MainLayout>
       <div className="w-[100%] bg-[#f1f2f8] min-h-[calc(100vh-64px)] py-[30px] flex items-center">
@@ -9,10 +26,93 @@ export default function RequestWritePage() {
           <h1 className="text-[24px] font-bold flex flex-col mb-[30px]">
             의뢰서 직접 작성
           </h1>
-          <textarea
-            className="w-[100%] h-[280px] bg-[#F7F8FC] rounded-[20px] resize-none py-[26px] px-[31px] text-black font-[16px] border-[1px] border-transparent outline-none focus:border-[1px] focus:border-[#d6d6d694] mb-[282px]"
-            placeholder="의뢰 내용을 작성해주세요."
-          ></textarea>
+          <div className="overflow-y-auto flex flex-col gap-[27px] pr-[31px] custom-scrollbar mb-10">
+            <div>
+              <h1 className="text-[#525466] text-[17px] mb-2">
+                <span className="text-red-600">*</span> 프로젝트 제목
+              </h1>
+              <input
+                type="text"
+                className="bg-[#F7F8FC] rounded-[8px] h-[40px] w-[100%] text-black font-[16px] border-[1px] border-transparent outline-none focus:border-[1px] focus:border-[#d6d6d694] px-4 text-[15px]"
+                value={projectTitle}
+                onChange={(e) => setProjectTitle(e.target.value)}
+              />
+            </div>
+            <div className="relative">
+              <h1 className="text-[#525466] text-[17px] mb-2">
+                <span className="text-red-600">*</span> 지출 가능 예산
+              </h1>
+              <input
+                type="text"
+                className="w-[200px] bg-[#F7F8FC] rounded-[8px] h-[40px] text-black font-[16px] border-[1px] border-transparent outline-none focus:border-[1px] focus:border-[#d6d6d694] 
+                pl-8 text-[15px]"
+                value={money}
+                onChange={(e) => setMoney(e.target.value)}
+              />
+              <p className="text-[#B0B3C6] text-[14px] absolute bottom-2 left-[10px]">
+                ₩
+              </p>
+            </div>
+            <div className="flex items-end">
+              <div>
+                <h1 className="text-[#525466] text-[17px] mb-2">
+                  <span className="text-red-600">*</span> 예상 시작일
+                </h1>
+                <DatePicker
+                  shouldCloseOnSelect
+                  selected={startDate}
+                  onChange={(data) => setStartDate(data)}
+                  startDate={startDate}
+                  endDate={endDate}
+                  placeholderText="YYYY. MM. DD"
+                  dateFormat="yyyy. MM. dd"
+                  className="bg-[#F7F8FC] rounded-[8px] h-[40px] w-[121px] text-[14px] text-center outline-none focus:border-[1px] focus:border-[#d6d6d694]"
+                  popperPlacement="bottom-start"
+                  popperClassName="z-[9999]"
+                />
+              </div>
+              <p className="text-[#B0B3C6] text-[30px] mx-[25px]">-</p>
+              <div>
+                <h1 className="text-[#525466] text-[17px] mb-2">마감일</h1>
+                <DatePicker
+                  selected={endDate}
+                  onChange={(data) => setEndDate(data)}
+                  startDate={startDate}
+                  endDate={endDate}
+                  placeholderText="YYYY. MM. DD"
+                  dateFormat="yyyy. MM. dd"
+                  className="bg-[#F7F8FC] rounded-[8px] h-[40px] w-[121px] text-[14px] text-center outline-none focus:border-[1px] focus:border-[#d6d6d694]"
+                  popperPlacement="bottom-start"
+                  popperClassName="z-[9999]"
+                />
+              </div>
+            </div>
+            <div>
+              <h1 className="text-[#525466] text-[17px]">
+                <span className="text-red-600">*</span> 상세 요구 사항
+              </h1>
+              <p className="text-[#525466] text-[12px] mb-[6px]">
+                프로젝트 개요 / 주요 기능(화면 구성, 필수 페이지 등) / 톤앤매너
+                / 우대사항 등을 알려주세요.
+              </p>
+              <textarea
+                className="w-[100%] h-[125px] bg-[#F7F8FC] rounded-[20px] resize-none py-[10px] px-4 text-black text-[15px] border-[1px] border-transparent outline-none focus:border-[1px] focus:border-[#d6d6d694]"
+                value={detail}
+                onChange={(e) => setDetail(e.target.value)}
+              ></textarea>
+            </div>
+            <div className="mb-[140px]">
+              <h1 className="text-[#525466] text-[17px]">
+                <span className="text-red-600">*</span> 참고 자료
+              </h1>
+              <p className="text-[#525466] text-[12px] mb-[6px]">
+                레퍼런스나 필요한 자료를 추가해주세요.
+              </p>
+              <button className="bg-[#F7F8FC] w-[100%] h-[40px] border-[1px] border-[#B0B3C6] rounded-[8px] cursor-pointer text-[#B0B3C6] text-[24px] text-center">
+                +
+              </button>
+            </div>
+          </div>
           <div className="flex justify-between items-center">
             <Link
               to="/request/category"
@@ -22,7 +122,7 @@ export default function RequestWritePage() {
             </Link>
             <Link
               to="/request/AI-question"
-              className="bg-[#BDCFFF] px-[17px] py-[8px] rounded-[8px] text-[18px] cursor-pointer"
+              className={`${warning ? 'bg-gray-300 pointer-events-none' : 'bg-[#BDCFFF]'} px-[17px] py-[8px] rounded-[8px] text-[18px] cursor-pointer`}
             >
               다음
             </Link>
