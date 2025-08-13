cfd60de0d68e26ff0951f9a5055f095aab5618ad
daeun <rlaekdms901@naver.com>
2025-08-13 19:34:40 +0900

    feat: 최종 결과 제출 페이지 UI 구현

--- src/pages/Designer/Submit/SubmitRequestModal.jsx
+++ src/pages/Designer/Submit/SubmitRequestModal.jsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import closeBtn from '../../../assets/RequestList/close-button.png';
 import moreBtn from '../../../assets/Designer/more.png';
 import folderBtn from '../../../assets/Designer/folder.png';
 
 export default function SubmitRequestModal({ submitModalOpen, onClose }) {
   if (!submitModalOpen) return null;
+  const [imgUploadBtn, setImgUploadBtn] = useState(false);
   return (
     <div
       onClick={onClose}
@@ -26,9 +28,10 @@ export default function SubmitRequestModal({ submitModalOpen, onClose }) {
         </div>
         <div className="w-[100%] h-[100%] flex flex-col pr-[36px]">
           <div className="flex items-end md:gap-[11px] mx-[1%] justify-between">
-            <h1 className="text-[#525466] text-[13px] sm:text-[18px] md:text-[20px] font-medium ">
-              와이어프레임 초안 1차 색상안 2종 포함
-            </h1>
+            <input
+              type="text"
+              className="w-[60%] h-[45px] bg-[#F2F3FA] px-[22px] py-[11px] rounded-[19px] text-[#525466] text-[20px] font-medium outline-none"
+            />
             <p className="text-[#525466] text-[13px] sm:text-[16px] font-light">
               2025.08.23
             </p>
@@ -37,16 +40,26 @@ export default function SubmitRequestModal({ submitModalOpen, onClose }) {
           <div className="w-[100%] h-[100%] flex flex-col ">
             <div className="w-[100%] h-[50%] flex flex-col pl-[2%] gap-[10px] pt-[10px] mb-[28px]">
               <p className="text-[16px] text-[#525466]">미리보기 사진</p>
-              <div className="w-[100%] h-[100%] flex justify-between gap-[10px] ">
+              <div className="w-[100%] h-[100%] flex justify-between gap-[10px]">
                 <div className="w-[95%] flex justify-between">
                   <div className="w-[45%] h-[95%] bg-[#DFE1ED] rounded-[10px]"></div>
                   <div className="w-[45%] h-[95%] bg-[#DFE1ED] rounded-[10px]"></div>
                 </div>
-                <img
-                  src={moreBtn}
-                  alt="moreBtn"
-                  className="w-[25px] h-[25px]"
-                />
+                <button
+                  onClick={() => setImgUploadBtn(!imgUploadBtn)}
+                  className="cursor-pointer flex items-start"
+                >
+                  <img
+                    src={moreBtn}
+                    alt="moreBtn"
+                    className="w-[25px] h-[25px]"
+                  />
+                </button>
+                {imgUploadBtn && (
+                  <button className="fixed right-55 top-71 bg-white shadow-lg w-[130px] h-[40px] rounded-[20px] cursor-pointer">
+                    <h1 className="text-[#525466] text-[14px]">사진 업로드 </h1>
+                  </button>
+                )}
               </div>
             </div>
             <div className="w-[100%] h-[33%]">
